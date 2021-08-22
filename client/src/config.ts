d69094bad267413e0d8499e97899a9699c92dbf6
Kingsley onyelo <45916833+kingsoo123@users.noreply.github.com>
2021-08-22 19:12:04 +0100

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
-const apiId = 'd5moervel7'
-export const apiEndpoint = `https://${apiId}.execute-api.ap-us-east-1.amazonaws.com/dev`
+//const apiId = 'd5moervel7'
+export const apiEndpoint = ` https://s192fakv93.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'dev-7b-xd3x5.us.auth0.com',
