11bc7ab2985fc00f5749011b5fda56303c5a656e
kingsoo123 <sabistore12@gmail.com>
2021-08-22 02:57:47 +0100

    firt commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'd5moervel7'
+export const apiEndpoint = `https://${apiId}.execute-api.ap-southeast-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-7b-xd3x5.us.auth0.com',
+  clientId: 'cnTRBUrSK4dVW1W7p6AVwdsjPjvhTPsJ',
+  callbackUrl: 'http://localhost:3000/callback'
+}
